fd6696189b56971803bbfec014102936ef8018ca
k0l0apj <k0l0apj@m-ddxpm7p360.attlocal.net>
2022-11-11 17:58:08 -0600

    fetch API to get deals to show

--- BakeSaleApp/src/components/App.js
+++ BakeSaleApp/src/components/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ajax from '../ajax'
+import DealList from './DealList';
 
 class App extends React.Component {
+    state = {
+        deals: [],
+    }
+    async componentDidMount(){
+        const deals = await ajax.fetchInitialDeals();
+        this.setState({ deals })
+        console.log(deals)
+    }
     render() {
         return(
             <View style={styles.container}>
-                <Text style={styles.header}>Bake Sale</Text>
+                {
+                    this.state.deals.length > 0 ?
+                    <DealList deals={this.state.deals}/>: 
+                    <Text style={styles.header}>Bake Sale</Text>
+                }
             </View>
         )
     }
